bdeac2c2e6f771bad10366ef7eface6f4be36792
B-Kral <benkral@icloud.com>
2024-06-08 23:06:54 +0100

    lets try async script

--- app/react-test/page.tsx
+++ app/react-test/page.tsx
@@ -27,8 +27,10 @@ export default function HomePage() {
         <div className="container">
         <div className='screenSelection'>
           <section>
+            <p>you need to reload the ge to get the game to show up <br />
+            (controls are left and right arrow keys, and f to fire) <br />
+            menu bar not currently functional</p>
             <h2>Menu bar shenanigans</h2>
-            <p>not currently functional</p>
             <ul>
               <li><button>Space Invader</button></li> {
               //onClick={SpaceInvaders}
@@ -39,7 +41,7 @@ export default function HomePage() {
             </ul>
           </section>
           <canvas id="screen" width="500" height="300"/>
-            <script id="gameScript" src="Space Invader.js"></script>
+            <script id="gameScript" src="Space Invader.js" async/>
         </div>
           <section>
             <h2>React Tests</h2>
