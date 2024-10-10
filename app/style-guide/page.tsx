bb9787edc5520c926fcbac2a7993590b2c47ad61
B-Kral <benkral@icloud.com>
2024-10-10 12:17:50 +0100

    completed style guide

--- app/style-guide/page.tsx
+++ app/style-guide/page.tsx
@@ -4,7 +4,7 @@ export default function main() {
       <h1>Style Guide</h1>
       <section>
         <h2>Colours</h2>
-        <div className="colours grid grid-cols-3 gap-2 p-10">
+        <div className="colours grid grid-cols-3 gap-2 p-10 md:grid-cols-4">
           <div className="colour-box">
             <div className="p-5 mb-5 bg-themePrimary text-themeText">
               <h3>
@@ -62,6 +62,44 @@ export default function main() {
           </div>
         </div>
       </section>
+      <div className="grid grid-cols-2">
+      <section className="styles">
+          <h2>Styles</h2>
+          <h1 className="text-left pt-0 pb-1">h1: Page head</h1>
+          <ul>
+            <li><a>font-weight</a>: bold</li>
+            <li><a>font-size</a>: 300%</li>
+            <li><a>color</a>: #856E99</li>
+            <li><a>text-transform</a>: uppercase</li>
+          </ul>
+          <h2 className="demo pt-5 pb-1">H2: section head</h2>
+          <ul>
+            <li><a>font-size</a>: x-large</li>
+            <li><a>font-weight</a>: bold</li>
+            <li><a>color</a>: #856E99</li>
+            <li>on section highlight: <a>color</a>: #FFB3E3</li>
+          </ul>
+          <h3 className="pt-5 pb-1">H3: subheading</h3>
+          <ul>
+          <li><a>font-size</a>: large</li>
+	        <li><a>font-weight</a>: bold</li>
+          <li><a>color</a>: #9F9F9F</li>
+	        <li><a>text-decoration</a>: underline</li>
+	        <li><a>text-decoration-color</a>: #856E99</li>
+          </ul>
+          <p className="pt-5 pb-2">p: paragraph text</p>
+          <ul>
+            <li><a>color</a>: #9F9F9F</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Fonts</h2>
+          <h3>Arial</h3>
+          <p>Sphinx of black quartz, judge my vow</p>
+          <p><strong>Sphinx of black quartz, judge my vow</strong></p>
+          <p><em>Sphinx of black quartz, judge my vow</em></p>
+        </section>
+      </div>
     </div>
   );
 }
