29fa0ae26dfc934da333c06ca43b215c0d3f3dbb
B-Kral <benkral@icloud.com>
2025-02-18 14:33:38 +0000

    fixed like-button path

--- app/games/adventure-game/layout.tsx
+++ app/games/adventure-game/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import LikeButton from "../ui/like-button";
+import LikeButton from "@/app/ui/like-button";
 import { useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -19,7 +19,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="grid grid-cols-5">
       <div className="p-5">
         <h3 className="text-themeSecondary text-xl font-bold py-1">Stats</h3>
-        <p>theres gonna be a bunch of stats and stuff here, maybe like a character bio or something aswell</p>
+        <p>
+          theres gonna be a bunch of stats and stuff here, maybe like a
+          character bio or something aswell
+        </p>
         <ul className="margintop-50">
           {stats.map((stat) => (
             <li key={stat[0]}>
@@ -31,13 +34,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="col-span-3">{children}</div>
       <div className="p-5">
         <p>
-          I dont really know what goes here, maybe navigation, or more stats, we'll work something out, in the meantime
-          have a like button:
+          I dont really know what goes here, maybe navigation, or more stats,
+          we'll work something out, in the meantime have a like button:
         </p>
         <LikeButton />
         <p>
-          actually I think an inventory type thing might be good here, I'll need to work out how to do global variables
-          like in Godot though
+          actually I think an inventory type thing might be good here, I'll need
+          to work out how to do global variables like in Godot though
         </p>
       </div>
     </div>
